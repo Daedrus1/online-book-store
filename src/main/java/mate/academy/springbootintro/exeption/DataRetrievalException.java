dc740b705b8ace0856a3efb69257772c254b8ac6
Pavlo Mykhailyk <pmwonderchrono@gmail.com>
2025-08-29 18:14:57 +0200

    fix 2.0

--- src/main/java/mate/academy/springbootintro/exeption/DataRetrievalException.java
+++
@@ -1,7 +0,0 @@
-package mate.academy.springbootintro.exeption;
-
-public class DataRetrievalException extends RuntimeException {
-    public DataRetrievalException(String message, Throwable cause) {
-        super(message, cause);
-    }
-}
--- src/main/java/mate/academy/springbootintro/repository/BookRepositoryImpl.java
+++ src/main/java/mate/academy/springbootintro/repository/BookRepositoryImpl.java
@@ -1,11 +1,12 @@
 package mate.academy.springbootintro.repository;
 
 import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
 import jakarta.persistence.PersistenceContext;
 import jakarta.persistence.PersistenceException;
 import jakarta.transaction.Transactional;
 import java.util.List;
-import mate.academy.springbootintro.exeption.DataRetrievalException;
+import mate.academy.springbootintro.exeption.DataProcessingException;
 import mate.academy.springbootintro.model.Book;
 import org.springframework.stereotype.Repository;
 
@@ -13,30 +14,36 @@ import org.springframework.stereotype.Repository;
 @Transactional
 public class BookRepositoryImpl implements BookRepository {
 
-    @PersistenceContext
-    private EntityManager em;
+    private final EntityManagerFactory entityManagerFactory;
+
+    public BookRepositoryImpl(EntityManagerFactory entityManagerFactory) {
+        this.entityManagerFactory = entityManagerFactory;
+    }
 
     @Override
     public Book save(Book book) {
-        try {
+        try (EntityManager em = entityManagerFactory.createEntityManager()) {
+            em.getTransaction().begin();
             if (book.getId() == null) {
                 em.persist(book);
-                return book;
             } else {
-                return em.merge(book);
+                book = em.merge(book);
             }
+            em.getTransaction().commit();
+            return book;
         } catch (PersistenceException e) {
-            throw new DataRetrievalException("Unable to save book: " + book, e);
+            throw new DataProcessingException("Unable to save book: " + book, e);
         }
     }
 
     @Override
     public List<Book> findAll() {
-        try {
+        try (EntityManager em = entityManagerFactory.createEntityManager()) {
             return em.createQuery("SELECT b FROM Book b", Book.class)
                     .getResultList();
         } catch (PersistenceException e) {
-            throw new DataRetrievalException("Unable to retrieve books", e);
+            throw new DataProcessingException("Unable to retrieve books", e);
         }
     }
-}
+    }
+
