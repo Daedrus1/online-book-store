bcfd1117f3701a2d27cba6a524f6a741cb8c28a0
Pavlo Mykhailyk <pmwonderchrono@gmail.com>
2025-08-30 18:20:18 +0200

    fix 3.0  add SpringbootIntroApplication class

---
+++ src/main/java/mate/academy/springbootintro/SpringbootIntroApplication.java
@@ -0,0 +1,27 @@
+package mate.academy.springbootintro;
+
+import mate.academy.springbootintro.model.Book;
+import mate.academy.springbootintro.repository.BookRepository;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class SpringbootIntroApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootIntroApplication.class, args);
+    }
+
+    @Bean
+    public CommandLineRunner demo(BookRepository bookRepository) {
+        return args -> {
+            Book book = new Book();
+            book.setTitle("Spring in Action");
+            bookRepository.save(book);
+
+            System.out.println("Books in DB: " + bookRepository.findAll());
+        };
+    }
+}
